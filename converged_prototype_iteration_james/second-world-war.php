f7cc64316f3a9936f28786980e48a1c76acf925a
JamesWChan <jwchan1993@gmail.com>
2020-11-25 12:33:38 +0000

    Refine prototype

--- converged_prototype_iteration_james/second-world-war.php
+++ converged_prototype_iteration_james/second-world-war.php
@@ -6,9 +6,11 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css"
           integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
     <link rel="stylesheet" href="styles/second-world-war.css">
+    <link rel="stylesheet" href="styles/style.css">
     <link rel='stylesheet' id='tna-google-fonts-css'
           href='https://fonts.googleapis.com/css?family=Open+Sans%3A400%2C400i%2C700%2C700i%7CRoboto%3A400%2C700&#038;display=swap&#038;ver=5.4.2'
           type='text/css' media='all'/>
+    <link rel="stylesheet" id="tna-google-fonts-css" href="https://fonts.googleapis.com/css?family=Open+Sans%3A400%2C400i%2C700%2C700i%7CRoboto+Mono%3A400%2C700&amp;display=swap&amp;ver=5.4.4" type="text/css" media="all">
 </head>
 <body class="css">
     <a href="index.php" class="tna-button">
@@ -27,7 +29,7 @@
             <!-- Three columns of text below the carousel -->
 
             <div class="showcase">
-                <h2 class="text-center">Featured records during this period</h2>
+                <h2 class="text-center">Most popular records during this period</h2>
                 <div class="row">
                     <div class="col-lg-4">
                         <div class="card">
@@ -54,27 +56,6 @@
                         </div>
                     </div>
                 </div><!-- /.row -->
-                <p class="text-center"><a href="" title="">See all our featured documents &gt;</a></p>
-            </div>
-
-
-            <h2 class="text-center mt-5">Refine your dates</h2>
-            <div class="center">
-                <form class="form-inline">
-                    <label class="sr-only" for="dateFrom">From</label>
-                    <input type="text" class="form-control mb-2 mr-sm-2" id="dateFrom" placeholder="1939">
-
-                    <label class="sr-only" for="dateTo">To</label>
-                    <div class="input-group mb-2 mr-sm-2">
-                        <input type="text" class="form-control" id="dateTo" placeholder="1945">
-                    </div>
-
-                    <div class="form-check mb-2 mr-sm-2">
-
-                    </div>
-
-                    <button type="submit" class="btn btn-primary mb-2">Refine</button>
-                </form>
             </div>
 
             <div id="text-visualisation">
@@ -86,6 +67,61 @@
                 </ul>
             </div>
 
+            <div class="tna-button refine-results">
+                <span>Refine results</span>
+            </div>
+
+            <div class="refine-results-form">
+                <form class="tna-form" action="second-world-war.php" method="get">
+                    <fieldset>
+                        <div class="form-section-title">
+                            <span>Filters</span>
+                        </div>
+                        <div class="tna-form__row">
+                            <div class="filter">
+                                <label for="digitised">Digitised</label>
+                                <input type="checkbox" id="digitised" name="digitised"
+                                       value="">
+                            </div>
+                            <div class="filter">
+                                <label for="filter-1">Filter 1</label>
+                                <input type="checkbox" id="filter-1" name="filter-1"
+                                       value="">
+                            </div>
+                            <div class="filter">
+                                <label for="filter-2">Filter 2</label>
+                                <input type="checkbox" id="filter-2" name="filter-2"
+                                       value="">
+                            </div>
+                            <div class="filter">
+                                <label for="filter-3">Filter 3</label>
+                                <input type="checkbox" id="filter-3" name="filter-3"
+                                       value="">
+                            </div>
+                        </div>
+                    </fieldset>
+                    <fieldset>
+                        <div class="form-section-title">
+                            <span>Refine years</span>
+                        </div>
+                        <div class="tna-form__row">
+                            <label for="start-year">Start year</label>
+                            <input type="text" pattern="\d{4}"  id="start-year" name="start-year" placeholder="YYYY"
+                                   value="">
+                        </div>
+                        <div class="tna-form__row">
+                            <label for="end-year">End year</label>
+                            <input type="text" pattern="\d{4}" id="end-year" name="end_year" placeholder="YYYY"
+                                   value="">
+                        </div>
+                    </fieldset>
+                    <fieldset>
+                        <div class="tna-form__row">
+                            <input class="tna-button" id="submit-btn" type="submit" value="Refine">
+                        </div>
+                    </fieldset>
+                </form>
+            </div>
 
             <h3>167,837 records available</h3>
             <div class="masonry">
@@ -131,6 +167,11 @@
     </main>
 </body>
 <footer>
-    <script src="script.js"></script>
+    <script src="scripts/script.js"></script>
+    <script
+            src="https://code.jquery.com/jquery-3.5.1.min.js"
+            integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0="
+            crossorigin="anonymous"></script>
+    <script src="scripts/refine.js"></script>
 </footer>
 </html>
