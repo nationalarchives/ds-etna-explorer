2b6b61c44345e2a75ec6a5f7854fa88a22b64208
AshTNA <ashleyaburnett@gmail.com>
2021-01-18 15:57:29 +0000

    Add images to index

--- main_prototype_iteration_two/index.php
+++ main_prototype_iteration_two/index.php
@@ -1,26 +1,9 @@
 <!doctype html>
 <html lang="en">
-<?php include 'includes/head.php' ?>
 <?php
-
-function render_card($era)
-{
-    include 'data/era_data.php';
-
-    $description = $era_descriptions[$era]; // from include
-    $time_period = $era_time_periods[$era]; // from include
-    $h3_era_text = ucfirst($era);
-    echo "<div class='col-lg-6'>
-    <div class='card'>
-        <div class='card-body'>
-            <h3 class='card-title'><a href='/results.php?featured_first=true&hide_records_without_image=true&era=$era'>$h3_era_text</a></h3>
-            <h4 class='card-subtitle mb-2 text-muted'>$time_period</h4>
-            <p class='card-text'>$description</p>
-        </div>
-    </div>
-</div>";
-}
-
+include_once 'includes/head.php';
+include_once 'includes/functions.php';
+include_once 'data/era_data.php';
 ?>
 
 <body>
@@ -35,9 +18,7 @@ function render_card($era)
                             <h1>Explore the collection through time</h1>
                         </div>
                         <div class="tna-page__entry">
-                            <p>From The Domesday Book to recent Cabinet papers, 1,000 years of international history are
-                                preserved in millions of records. Explore wars, revolutions, life stories and landmark
-                                rulings, and iconic figures including Shakespeare, Queen Victoria, Gandhi and Churchill.</p>
+                            <p><?php echo $era_summary ?></p>
                         </div>
                         <h2>Explore by time period</h2>
                         <p>Our experts have identified eight consecutive time periods, from medieval to postwar,
@@ -54,9 +35,6 @@ function render_card($era)
                             render_card("postwar");
                             ?>
                         </div>
-                        <h3>Choose your own dates</h3>
-                        <p>Select your own date ranges to explore the collection:</p>
-                        <?php include 'includes/date_form.php' ?>
             </div>
             </article>
             </main>
