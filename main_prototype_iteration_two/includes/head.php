fa6033865315a334a43efb9e417af2c1aa70688d
AshTNA <ashleyaburnett@gmail.com>
2021-01-18 13:53:38 +0000

    Fix cookie error when cookie isn't set

--- main_prototype_iteration_two/includes/head.php
+++ main_prototype_iteration_two/includes/head.php
@@ -1,13 +1,16 @@
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    
-    
-    <?php
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+      <meta http-equiv="X-UA-Compatible" content="ie=edge">
 
-      if($_COOKIE["disable_enhancements"] != "true") {
+
+      <?php
+
+      if (!isset($_COOKIE['disabled_enhancements'])) {
+            $_COOKIE['disabled_enhancements'] = "false";
+      }
+
+      if ($_COOKIE["disable_enhancements"] != "true") {
             echo '
             <!-- Google fonts -->
     <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,400i,700,700i|Roboto+Mono:400,700&display=swap"
@@ -21,7 +24,7 @@
             <link rel="stylesheet" href="/styles/mb.css">
             <link rel="stylesheet" href="/styles/main.css">';
       }
-    ?>
+      ?>
 
-    <title>Collection Explorer</title>
+      <title>Collection Explorer</title>
 </head>
